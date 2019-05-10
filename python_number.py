14107a0387afd90d588e85b404c346dcaadd3ff6
anilkum5319 <kareadla@gmail.com>
2019-05-10 13:12:04 -0400

    python_number

---
+++ python_number.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+############################### Python Numbers  #########################
+
+#There are three numeric types in Python:
+#int
+#float
+#complex
+#Variables of numeric types are created when you assign a value to them:
+
+#### Int
+# 	Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+##### Example #
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
