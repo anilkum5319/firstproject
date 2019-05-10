4f5a2b9e5218e1750ce15ac1d91512400bac7461
anilkum5319 <kareadla@gmail.com>
2019-05-10 13:04:14 -0400

    PYTHON-SCRIPT

---
+++ variable.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+x = "awesome"
+print ("Python is " " " +  x)
+
+x = "Python is "
+y = "awesome"
+z =  x + y
+print(z)
+
+#The Python print statement is often used to output variables.To combine both text and a variable, Python uses the + character:
+#Unlike other programming languages, Python has no command for declaring a variable. A variable is created the moment you first assign a value to it.
+#Variables do not need to be declared with any particular type and can even change type after they have been set.
+
+################Rules for Python variables:######################
+#A variable name must start with a letter or the underscore character
+#A variable name cannot start with a number
+#A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+#Variable names are case-sensitive (age, Age and AGE are three different variables)
