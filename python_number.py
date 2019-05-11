2690af586e9f3ef89731558cfdc2a66e700a883a
anilkum5319 <kareadla@gmail.com>
2019-05-11 06:30:09 -0400

    Python_Number

--- python_number.py
+++ python_number.py
@@ -24,9 +24,42 @@ print(type(z))
 #### Int
 # 	Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
 ##### Example #
+
+print ("This Example for Integer::-")
+
 x = 1
-y = 35656222554887711
-z = -3255522
+y = 5319531953195319
+z = -5319
+
+print(type(x))
+print(type(y))
+print(type(z)) 
+
+###################### Float #####################
+
+#*** Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+
+#^^^^^^^ Example ^^^^^^^^^^^
+
+print ("This Example for Floats::-")
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+###################### Complex ###############
+
+# Complex numbers are written with a "j" as the imaginary part:
+
+print ("This Example for Complex ::-")
+
+x = 3+5j
+y = 5j
+z = -5j
 
 print(type(x))
 print(type(y))
