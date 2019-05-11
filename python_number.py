ebe28dab0ed9d5aa7df005bf64eeef651d2a7a65
Anil Kumar <50414974+anilkum5319@users.noreply.github.com>
2019-05-11 15:19:05 +0530

    git commit -m "Python_Numbers"

--- python_number.py
+++ python_number.py
@@ -8,6 +8,19 @@
 #complex
 #Variables of numeric types are created when you assign a value to them:
 
+##### Example #
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#To verify the type of any object in Python, use the type() function:
+    
+##### Example #
+print(type(x))
+print(type(y))
+print(type(z))
+
 #### Int
 # 	Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
 ##### Example #
