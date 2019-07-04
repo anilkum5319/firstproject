121f1c73b158475f2d2fe144ed3a636029ec2bdd
anilkum5319 <kareadla@gmail.com>
2019-07-04 06:43:27 -0400

    if_else and 1 to 100

---
+++ 1and100.py
@@ -0,0 +1,8 @@
+#Program to check, between value 1 to 100
+
+#!/usr/bin/python
+x=int(input("Enter The Value:"))
+if 1<=x<=100:
+      print("The Number",x," Is Between 1 to 100")
+else:
+      print("The Number",x," Is Not Between 1 to 100")
---
+++ if_else.py
@@ -0,0 +1,17 @@
+# To find biggest of given two numbers from the key board
+
+#!/usr/bin/python
+
+#x=int(input("Enter The 1st Number:"))
+#y=int(input("Enter The 2nd Number:"))
+#z=int(input("Enter The 3rd Number:"))
+#if x>y and x>z:
+  #print("The Bigger Number is:",x)
+#else:
+  # print("The Smaler Number is:",y)
+  #print("The Smalest Number is:",z)
+
+#if x<y:
+  # print("The Smalest Number is:",x)
+#elif y>z:
+ #print("The Smalest Value is:",z)
