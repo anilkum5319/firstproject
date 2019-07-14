4b2fd3671b987a61b3e88078e9470ddd0a924c04
anilkum5319 <kareadla@gmail.com>
2019-07-13 22:13:37 -0400

    username

---
+++ username_Passwd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+name=""
+pwd=""
+
+while name!='Anilkum5319' and pwd!='anilkum':
+   name=input("Enter The User Name:")
+   pwd=input("Enter The User Passwd:")
+print("User Name and Passwd, Both are Matched!")   
+#else:
+ #print("User Name and Passwd, Both are Matched!")
+ #print("User Name and Passwd, are not Matched!")
+
+#print("User Name and Passwd, Both are Matched!")
